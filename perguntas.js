fb699d34b49b054c621237cf6e1f95159d5dec55
mileneangeleli <167561925+mileneangeleli@users.noreply.github.com>
2025-07-07 11:02:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'MATEMÁTICA',
+    'Resolva a equação 3x + 2 = 11.',
+    'x= 3'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome do rio que atravessa Londres?',
+    'O rio Tamisa atravessa Londres.'
+)
+
+criaCartao(
+    'MATEMÁTICA',
+    'Qual é a fórmula para calcular a área de um círculo?',
+    'A = π * r²'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'What are your goals for the future? ',
+    'Quais são seus objetivos para o futuro'
+)
